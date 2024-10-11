a487f51323bcc62f7b95d4f4012643825f7fb590
MDJAmin <mohammadamindf@gmail.com>
2024-10-11 10:49:00 +0330

    Feat : Add variable in JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,58 @@
-"use strict"
+
+window.addEventListener("load", event => {
+
+// - - - Variable - - - //
+
+    function productHeading() {
+        const product = {
+
+            value: 125,
+            images: [{
+                    img: '../assets/img01.png'
+                },
+                {
+                    img: '../assets/img02.png'
+                },
+                {
+                    img: '../assets/img03.png'
+                },
+                {
+                    img: '../assets/img04.png'
+                },
+                {
+                    img: '../assets/img05.png'
+                },
+                {
+                    img: '../assets/img06.png'
+                }
+            ]
+        }
+
+        const btnAdd = document.querySelector('.btn.add'),
+            btnContainer = document.querySelector('.btnContainer'),
+            wrapper = document.querySelector('.wrapper'),
+            itemNumber = document.querySelector('.itemNumber'),
+            shoppingQuantity = document.querySelector('.shoppingQuantity'),
+            inputQuantity = document.querySelector('.inputQuantity'),
+            plus = document.querySelector('.plus'),
+            minus = document.querySelector('.minus'),
+            arrowDrop = document.querySelector('.arrowDrop'),
+            dropdown = document.querySelector('.dropdown'),
+            nav = document.querySelector('nav'),
+            error = document.querySelector('.error'),
+            shoppingIcon = document.querySelector('.shoppingIcon'),
+            shoppingMenu = document.querySelector('.shoppingMenu'),
+            emptyCart = document.querySelector('.emptyCart');
+
+        let = priceFinal = document.querySelector('.priceFinal'),
+            priceOriginal = document.querySelector('.priceOriginal'),
+            discount = null,
+            sizeNumber = document.querySelector('.sizeNumber'),
+            dropItem = document.querySelectorAll('.dropItem'),
+            maxQuantity = 5,
+            newMaxQuantity = maxQuantity;
+
+    }
+
+    productHeading();
+});
