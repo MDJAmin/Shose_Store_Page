4cd8a82e19b25ba03961b72471c91428ed3201c6
MDJAmin <mohammadamindf@gmail.com>
2024-10-11 11:29:00 +0330

    Clean Codes

--- js/script.js
+++ js/script.js
@@ -1,212 +1,206 @@
+window.addEventListener("load", (event) => {
+  // - - - Variable - - - //
 
-window.addEventListener("load", event => {
+  function productHeading() {
+    const product = {
+      value: 125,
+      images: [
+        {
+          img: "../assets/img01.png",
+        },
+        {
+          img: "../assets/img02.png",
+        },
+        {
+          img: "../assets/img03.png",
+        },
+        {
+          img: "../assets/img04.png",
+        },
+        {
+          img: "../assets/img05.png",
+        },
+        {
+          img: "../assets/img06.png",
+        },
+      ],
+    };
 
-// - - - Variable - - - //
+    const btnAdd = document.querySelector(".btn.add"),
+      btnContainer = document.querySelector(".btnContainer"),
+      wrapper = document.querySelector(".wrapper"),
+      itemNumber = document.querySelector(".itemNumber"),
+      shoppingQuantity = document.querySelector(".shoppingQuantity"),
+      inputQuantity = document.querySelector(".inputQuantity"),
+      plus = document.querySelector(".plus"),
+      minus = document.querySelector(".minus"),
+      arrowDrop = document.querySelector(".arrowDrop"),
+      dropdown = document.querySelector(".dropdown"),
+      nav = document.querySelector("nav"),
+      error = document.querySelector(".error"),
+      shoppingIcon = document.querySelector(".shoppingIcon"),
+      shoppingMenu = document.querySelector(".shoppingMenu"),
+      emptyCart = document.querySelector(".emptyCart");
 
-    function productHeading() {
-        const product = {
+    (let = priceFinal = document.querySelector(".priceFinal")),
+      (priceOriginal = document.querySelector(".priceOriginal")),
+      (discount = null),
+      (sizeNumber = document.querySelector(".sizeNumber")),
+      (dropItem = document.querySelectorAll(".dropItem")),
+      (maxQuantity = 5),
+      (newMaxQuantity = maxQuantity);
 
-            value: 125,
-            images: [{
-                    img: '../assets/img01.png'
-                },
-                {
-                    img: '../assets/img02.png'
-                },
-                {
-                    img: '../assets/img03.png'
-                },
-                {
-                    img: '../assets/img04.png'
-                },
-                {
-                    img: '../assets/img05.png'
-                },
-                {
-                    img: '../assets/img06.png'
-                }
-            ]
+    // - - - Events - - - ///
+
+    btnAdd.addEventListener("click", addItem);
+    plus.addEventListener("click", plusQuantity);
+    minus.addEventListener("click", minusQuantity);
+    arrowDrop.addEventListener("click", openDrop);
+    shoppingIcon.addEventListener("click", openShoppingCart);
+
+    emptyCart.addEventListener("click", cleanCart);
+
+    dropItem.forEach(function (el) {
+      el.addEventListener("click", getSize);
+    });
+
+    window.addEventListener("resize", resize);
+
+    // - - - Functionality - - - //
+
+    // Fixed Nav
+
+    window.onscroll = function () {
+      if (window.pageYOffset >= 60) {
+        nav.classList.add("fixed");
+      } else {
+        nav.classList.remove("fixed");
+      }
+    };
+
+    // Change button position on mobile
+
+    function resize() {
+      //Button
+      if (window.innerHeight > wrapper.offsetHeight) {
+        btnContainer.classList.remove("fixedBtn");
+      } else {
+        btnContainer.classList.add("fixedBtn");
+      }
+      parallax();
+    }
+
+    // Parallax
+
+    function parallax() {
+      if (window.innerWidth > 800) {
+        var scene = document.querySelectorAll(".scene");
+        scene.forEach((pic) => {
+          var parallax = new Parallax(pic);
+        });
+      }
+    }
+
+    // Calculate the Discount
+
+    function getDisccount() {
+      priceOriginal.innerText = product.value + "€";
+      discount = product.value - product.value * (30 / 100);
+      priceFinal.innerText = discount + "€";
+    }
+
+    // Calculate the the Prices with discounts
+
+    function getPrice() {
+      priceFinal.innerText = discount * inputQuantity.value + "€";
+      priceOriginal.innerText = product.value * inputQuantity.value + "€";
+
+      setTimeout(() => {
+        priceFinal.classList.remove("anime");
+      }, 400);
+    }
+
+    // Update the prices with the quantity counter
+
+    function plusQuantity() {
+      if (inputQuantity.value < maxQuantity) {
+        inputQuantity.value == inputQuantity.value++;
+        priceFinal.classList.add("anime");
+      }
+      getPrice();
+    }
+
+    function minusQuantity() {
+      if (inputQuantity.value > 1) {
+        inputQuantity.value == inputQuantity.value--;
+        priceFinal.classList.add("anime");
+      }
+      getPrice();
+    }
+
+    // Add items to shopping cart
+
+    function addItem() {
+      let cenas =
+        parseInt(itemNumber.innerText) + parseInt(inputQuantity.value);
+
+      if (cenas <= newMaxQuantity) {
+        itemNumber.style.display = "flex";
+        itemNumber.innerText = cenas;
+        shoppingQuantity.innerText = "x" + cenas;
+        itemNumber.classList.add("addItem");
+        error.style.display = "none";
+      } else {
+        error.style.display = "flex";
+      }
+
+      setTimeout(() => {
+        itemNumber.classList.remove("addItem");
+      }, 700);
+    }
+
+    // Open Drop
+
+    function openDrop() {
+      if (dropdown.classList.contains("open")) {
+        dropdown.classList.remove("open");
+      } else {
+        dropdown.classList.add("open");
+      }
+    }
+
+    //get Drop Size Number Value
+
+    function getSize(e) {
+      sizeNumber.innerText = e.currentTarget.innerText;
+      openDrop();
+    }
+
+    // Open Shopphing cart
+
+    function openShoppingCart() {
+      if (itemNumber.innerText != "0") {
+        if (shoppingMenu.classList.contains("openShopping")) {
+          shoppingMenu.classList.remove("openShopping");
+        } else {
+          shoppingMenu.classList.add("openShopping");
         }
+      }
+    }
 
-        const btnAdd = document.querySelector('.btn.add'),
-            btnContainer = document.querySelector('.btnContainer'),
-            wrapper = document.querySelector('.wrapper'),
-            itemNumber = document.querySelector('.itemNumber'),
-            shoppingQuantity = document.querySelector('.shoppingQuantity'),
-            inputQuantity = document.querySelector('.inputQuantity'),
-            plus = document.querySelector('.plus'),
-            minus = document.querySelector('.minus'),
-            arrowDrop = document.querySelector('.arrowDrop'),
-            dropdown = document.querySelector('.dropdown'),
-            nav = document.querySelector('nav'),
-            error = document.querySelector('.error'),
-            shoppingIcon = document.querySelector('.shoppingIcon'),
-            shoppingMenu = document.querySelector('.shoppingMenu'),
-            emptyCart = document.querySelector('.emptyCart');
+    //Clean Shopping Cart
 
-        let = priceFinal = document.querySelector('.priceFinal'),
-            priceOriginal = document.querySelector('.priceOriginal'),
-            discount = null,
-            sizeNumber = document.querySelector('.sizeNumber'),
-            dropItem = document.querySelectorAll('.dropItem'),
-            maxQuantity = 5,
-            newMaxQuantity = maxQuantity;
+    function cleanCart() {
+      shoppingMenu.classList.remove("openShopping");
+      itemNumber.style.display = "none";
+      itemNumber.classList.remove("addItem");
+      itemNumber.innerText = "0";
+    }
 
-            
-            // - - - Events - - - ///
+    // Populate the images for Swiper
 
-
-        btnAdd.addEventListener('click', addItem);
-        plus.addEventListener("click", plusQuantity);
-        minus.addEventListener("click", minusQuantity);
-        arrowDrop.addEventListener("click", openDrop);
-        shoppingIcon.addEventListener("click", openShoppingCart);
-
-        emptyCart.addEventListener("click", cleanCart);
-
-        dropItem.forEach(function (el) {
-            el.addEventListener("click", getSize);
-        })
-
-        window.addEventListener("resize", resize);
-
-// - - - Functionality - - - //
-
-        // Fixed Nav 
-
-        window.onscroll = function () {
-            if (window.pageYOffset >= 60) {
-                nav.classList.add("fixed");
-            } else {
-                nav.classList.remove("fixed");
-            }
-        };
-
-        // Change button position on mobile
-
-        function resize() {
-            //Button
-            if (window.innerHeight > wrapper.offsetHeight) {
-                btnContainer.classList.remove('fixedBtn');
-            } else {
-                btnContainer.classList.add('fixedBtn');
-            }
-            parallax();
-        }
-
-        // Parallax
-
-        function parallax() {
-            if (window.innerWidth > 800) {
-                var scene = document.querySelectorAll('.scene');
-                scene.forEach(pic => {
-                    var parallax = new Parallax(pic);
-                })
-            }
-        }
-
-        // Calculate the Discount
-
-        function getDisccount() {
-            priceOriginal.innerText = product.value + "€";
-            discount = product.value - (product.value * (30 / 100));
-            priceFinal.innerText = discount + "€";
-        }
-
-        // Calculate the the Prices with discounts
-
-        function getPrice() {
-
-            priceFinal.innerText = discount * inputQuantity.value + "€";
-            priceOriginal.innerText = product.value * inputQuantity.value + "€";
-
-            setTimeout(() => {
-                priceFinal.classList.remove('anime');
-            }, 400);
-        }
-
-        // Update the prices with the quantity counter
-
-        function plusQuantity() {
-            if (inputQuantity.value < maxQuantity) {
-                inputQuantity.value == inputQuantity.value++;
-                priceFinal.classList.add('anime');
-            }
-            getPrice();
-        }
-
-        function minusQuantity() {
-            if (inputQuantity.value > 1) {
-                inputQuantity.value == inputQuantity.value--;
-                priceFinal.classList.add('anime');
-            }
-            getPrice();
-        }
-        
-        // Add items to shopping cart
-
-        function addItem() {
-
-            let cenas = parseInt(itemNumber.innerText) + parseInt(inputQuantity.value);
-
-            if (cenas <= newMaxQuantity) {
-                itemNumber.style.display = "flex";
-                itemNumber.innerText = cenas;
-                shoppingQuantity.innerText = "x" + cenas;
-                itemNumber.classList.add("addItem");
-                error.style.display = "none";
-            } else {
-                error.style.display = "flex";
-            }
-
-            setTimeout(() => {
-                itemNumber.classList.remove("addItem");
-            }, 700);
-        }
-
-        // Open Drop
-
-        function openDrop() {
-            if (dropdown.classList.contains('open')) {
-                dropdown.classList.remove('open');
-            } else {
-                dropdown.classList.add('open');
-            }
-        }
-
-        //get Drop Size Number Value 
-
-        function getSize(e) {
-            sizeNumber.innerText = e.currentTarget.innerText;
-            openDrop();
-        }
-
-        // Open Shopphing cart
-
-        function openShoppingCart() {
-            if (itemNumber.innerText != "0") {
-                if (shoppingMenu.classList.contains('openShopping')) {
-                    shoppingMenu.classList.remove('openShopping');
-                } else {
-                    shoppingMenu.classList.add('openShopping');
-                }
-            }
-        }
-
-        //Clean Shopping Cart
-
-        function cleanCart() {
-            shoppingMenu.classList.remove('openShopping');
-            itemNumber.style.display = "none";
-            itemNumber.classList.remove('addItem');
-            itemNumber.innerText = "0";
-        }
-
-        // Populate the images for Swiper
-
-        product.images.forEach(function (el) {
-
-            let template = `
+    product.images.forEach(function (el) {
+      let template = `
                 <div class="swiper-slide">
                     <div class="scene" data-hover-only="false"> 
                         <img src="${el.img}" data-depth="0.5">
@@ -214,61 +208,60 @@ window.addEventListener("load", event => {
                     </div>
                 </div>`;
 
-            let template2 = `
+      let template2 = `
                 <div class="swiper-slide">
                     <img src="${el.img}">
                 </div>`;
 
-            document.querySelector('.galleryMain .swiper-wrapper').insertAdjacentHTML('beforeend', template);
-            document.querySelector('.galleryThumbs .swiper-wrapper').insertAdjacentHTML('beforeend', template2);
-        });
+      document
+        .querySelector(".galleryMain .swiper-wrapper")
+        .insertAdjacentHTML("beforeend", template);
+      document
+        .querySelector(".galleryThumbs .swiper-wrapper")
+        .insertAdjacentHTML("beforeend", template2);
+    });
 
+    // Make the slider function
 
-        // Make the slider function
+    var galleryThumbs = new Swiper(".galleryThumbs", {
+      spaceBetween: 0,
+      slidesPerView: "auto",
+      loop: false,
+      allowTouchMove: false,
+      allowSlidePrev: false,
+      allowSlideNext: false,
+    });
 
-        var galleryThumbs = new Swiper('.galleryThumbs', {
-            spaceBetween: 0,
-            slidesPerView: 'auto',
-            loop: false,
-            allowTouchMove: false,
-            allowSlidePrev: false,
-            allowSlideNext: false,
+    var galleryMain = new Swiper(".galleryMain", {
+      spaceBetween: 300,
+      speed: 500,
+      loop: true,
+      loopedSlides: 5, //looped slides should be the same
+      effect: "coverflow",
+      coverflowEffect: {
+        rotate: 50,
+        slideShadows: false,
+        depth: 200,
+        stretch: 50,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      thumbs: {
+        swiper: galleryThumbs,
+      },
+    });
 
-        });
+    // Call functions
+    getDisccount();
+    parallax();
+    resize();
+  }
 
-        var galleryMain = new Swiper('.galleryMain', {
-            spaceBetween: 300,
-            speed: 500,
-            loop: true,
-            loopedSlides: 5, //looped slides should be the same
-            effect: "coverflow",
-            coverflowEffect: {
-                rotate: 50,
-                slideShadows: false,
-                depth: 200,
-                stretch: 50,
-
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-
-            },
-            thumbs: {
-                swiper: galleryThumbs,
-            },
-        });
-
-        // Call functions 
-        getDisccount();
-        parallax();
-        resize();
-
-    }
-
-    productHeading();
+  productHeading();
 });
