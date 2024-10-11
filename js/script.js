e757a7b4b2bd4d3c46721ab8009dc9cf20e50fe1
MDJAmin <mohammadamindf@gmail.com>
2024-10-11 11:28:02 +0330

    Feat : Complete the functionality

--- js/script.js
+++ js/script.js
@@ -142,6 +142,131 @@ window.addEventListener("load", event => {
             }
             getPrice();
         }
+        
+        // Add items to shopping cart
+
+        function addItem() {
+
+            let cenas = parseInt(itemNumber.innerText) + parseInt(inputQuantity.value);
+
+            if (cenas <= newMaxQuantity) {
+                itemNumber.style.display = "flex";
+                itemNumber.innerText = cenas;
+                shoppingQuantity.innerText = "x" + cenas;
+                itemNumber.classList.add("addItem");
+                error.style.display = "none";
+            } else {
+                error.style.display = "flex";
+            }
+
+            setTimeout(() => {
+                itemNumber.classList.remove("addItem");
+            }, 700);
+        }
+
+        // Open Drop
+
+        function openDrop() {
+            if (dropdown.classList.contains('open')) {
+                dropdown.classList.remove('open');
+            } else {
+                dropdown.classList.add('open');
+            }
+        }
+
+        //get Drop Size Number Value 
+
+        function getSize(e) {
+            sizeNumber.innerText = e.currentTarget.innerText;
+            openDrop();
+        }
+
+        // Open Shopphing cart
+
+        function openShoppingCart() {
+            if (itemNumber.innerText != "0") {
+                if (shoppingMenu.classList.contains('openShopping')) {
+                    shoppingMenu.classList.remove('openShopping');
+                } else {
+                    shoppingMenu.classList.add('openShopping');
+                }
+            }
+        }
+
+        //Clean Shopping Cart
+
+        function cleanCart() {
+            shoppingMenu.classList.remove('openShopping');
+            itemNumber.style.display = "none";
+            itemNumber.classList.remove('addItem');
+            itemNumber.innerText = "0";
+        }
+
+        // Populate the images for Swiper
+
+        product.images.forEach(function (el) {
+
+            let template = `
+                <div class="swiper-slide">
+                    <div class="scene" data-hover-only="false"> 
+                        <img src="${el.img}" data-depth="0.5">
+                        <img src="${el.img}" data-depth="1" class="shadow">
+                    </div>
+                </div>`;
+
+            let template2 = `
+                <div class="swiper-slide">
+                    <img src="${el.img}">
+                </div>`;
+
+            document.querySelector('.galleryMain .swiper-wrapper').insertAdjacentHTML('beforeend', template);
+            document.querySelector('.galleryThumbs .swiper-wrapper').insertAdjacentHTML('beforeend', template2);
+        });
+
+
+        // Make the slider function
+
+        var galleryThumbs = new Swiper('.galleryThumbs', {
+            spaceBetween: 0,
+            slidesPerView: 'auto',
+            loop: false,
+            allowTouchMove: false,
+            allowSlidePrev: false,
+            allowSlideNext: false,
+
+        });
+
+        var galleryMain = new Swiper('.galleryMain', {
+            spaceBetween: 300,
+            speed: 500,
+            loop: true,
+            loopedSlides: 5, //looped slides should be the same
+            effect: "coverflow",
+            coverflowEffect: {
+                rotate: 50,
+                slideShadows: false,
+                depth: 200,
+                stretch: 50,
+
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+
+            },
+            thumbs: {
+                swiper: galleryThumbs,
+            },
+        });
+
+        // Call functions 
+        getDisccount();
+        parallax();
+        resize();
 
     }
 
